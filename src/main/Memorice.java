d5849761fe6ef6796b0290cb81584c370761b275
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2026-04-15 18:57:10 -0700

    memorama v2

--- src/main/Memorice.java
+++ src/main/Memorice.java
@@ -8,13 +8,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.awt.event.ActionEvent;
@@ -23,6 +26,10 @@ public class Memorice {
 
 	private JFrame frame;
 	public Integer [] cartas = {1,1,2,2,3,3,4,4,5,5,6,6};
+	public int [] status = {0,0,0,0,0,0,0,0,0,0,0,0};
+	public int tarjetas_mostradas = 0;
+	public int carta_1=0,carta_2=0;
+	public List<JButton> botones = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -91,7 +98,9 @@ public class Memorice {
 		btnNewButton_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				btnNewButton_5.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/1.png")));
+				flip(0,btnNewButton_5);
+				
+				//btnNewButton_5.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/1.png")));
 			}
 		});
 		btnNewButton_5.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
@@ -101,41 +110,84 @@ public class Memorice {
 		JButton btnNewButton_3 = new JButton("");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				btnNewButton_3.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/1.png")));
+				
+				flip(1,btnNewButton_3);
+				
+				//btnNewButton_3.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/1.png")));
 			}
 		});
 		btnNewButton_3.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(2,btnNewButton_4);
+			}
+		});
 		btnNewButton_4.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_4);
 		
 		JButton btnNewButton_8 = new JButton("");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(3,btnNewButton_8);
+			}
+		});
 		btnNewButton_8.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_8);
 		
 		JButton btnNewButton_11 = new JButton("");
+		btnNewButton_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(4,btnNewButton_11);
+			}
+		});
 		btnNewButton_11.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_11);
 		
 		JButton btnNewButton_10 = new JButton("");
+		btnNewButton_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(5,btnNewButton_10);
+			}
+		});
 		btnNewButton_10.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_10);
 		
 		JButton btnNewButton_12 = new JButton("");
+		btnNewButton_12.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(6,btnNewButton_12);
+			}
+		});
 		btnNewButton_12.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_12);
 		
 		JButton btnNewButton_13 = new JButton("");
+		btnNewButton_13.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(7,btnNewButton_13);
+			}
+		});
 		btnNewButton_13.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_13);
 		
 		JButton btnNewButton_9 = new JButton("");
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(8,btnNewButton_9);
+			}
+		});
 		btnNewButton_9.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_9);
 		
 		JButton btnNewButton_7 = new JButton("");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				flip(9,btnNewButton_7);
+			}
+		});
 		btnNewButton_7.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
 		panel_1.add(btnNewButton_7);
 		
@@ -162,6 +214,11 @@ public class Memorice {
 		frame.getContentPane().add(panel_2, BorderLayout.SOUTH);
 		
 		JButton btnNewButton = new JButton("Reiniciar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		panel_2.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Pausa");
@@ -169,10 +226,77 @@ public class Memorice {
 	}
 	
 	public void flip(Integer n,JButton b) {
-	
-		System.out.println(cartas[n]);
 		
-		b.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/"+cartas[n]+".png")));
+		if(status[n] == 0) { 
+			
+			if(carta_1 == 0) {
+				carta_1 =   cartas[n];
+			}else {
+				carta_2 =  cartas[n];
+			}  
+			
+			status[n] = 1;
+			
+			
+			b.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/"+cartas[n]+".png")));
+			
+			botones.add(b);
+			
+			tarjetas_mostradas+= 1;
+			
+			if(tarjetas_mostradas == 2) {
+				
+				if (carta_1 == carta_2 && carta_1!=0) {
+					System.out.println("Pares");
+					
+					for (Iterator iterator = botones.iterator(); iterator.hasNext();) {
+						JButton jButton = (JButton) iterator.next(); 
+						jButton.setEnabled(false);
+					}
+					
+					botones.clear();
+					
+					tarjetas_mostradas = 0; 
+					carta_1 = 0;
+					carta_2 = 0; 
+					
+					for (int i = 0; i < 12; i++) {
+						
+						if(status[i]!=2) {
+							status[i]=0;
+						}
+					}
+					
+				}else {
+					
+					tarjetas_mostradas = 0; 
+					carta_1 = 0;
+					carta_2 = 0; 
+					
+					for (int i = 0; i < 12; i++) {
+						
+						if(status[i]!=2) {
+							status[i]=0;
+						}
+					}
+					
+					JOptionPane.showMessageDialog(null, "alert", "alert", JOptionPane.ERROR_MESSAGE);
+					
+					for (Iterator iterator = botones.iterator(); iterator.hasNext();) {
+						JButton jButton = (JButton) iterator.next();
+						jButton.setIcon(new ImageIcon(Memorice.class.getResource("/images/memorice/placeholder.png")));
+					}
+					
+					botones.clear();
+				}
+				
+			}
+			
+			
+		}else {
+			//System.out.println(status[n]);
+		}
+		
 	}
 	
 	
