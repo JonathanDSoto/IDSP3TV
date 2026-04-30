740aa50399c04a7a8c15d71c004e00921f6fa753
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2026-04-29 17:48:46 -0700

    mvc v2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,15 @@
 package main;
 
+import controllers.AuthController;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Ventana miVentana = new Ventana();
+		//Ventana miVentana = new Ventana();
+		AuthController ac = new AuthController();
+		
+		ac.showLogin();
 	}
 
 }
